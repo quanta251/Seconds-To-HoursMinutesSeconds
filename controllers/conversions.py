2c35113bc5cec33dfe06a4c51b1954a76ef639b1
Hayden Britt <haydenbritt25@gmail.com>
2024-06-10 10:42:01 -0500

    added seconds2days which converts seconds to days, hours, minutes, seconds

--- controllers/conversions.py
+++ controllers/conversions.py
@@ -9,14 +9,32 @@ def seconds2hours(seconds: int) -> list[int]:
     return [hours, remaining_minutes, remaining_seconds]
 
 
+def seconds2days(seconds: int) -> list[int]:
+    minutes: int = int(np.fix(seconds / 60))
+    hours: int = int(np.fix(minutes / 60))
+    days: int = int(np.fix(hours / 24))
+    remaining_seconds: int = seconds - (minutes * 60)
+    remaining_minutes: int = minutes - (hours * 60)
+    remaining_hours: int = hours - (days * 24)
+    return [days, remaining_hours, remaining_minutes, remaining_seconds]
+
+
 def main():
     mytime: int = 25478685
 
     time_list: list[int] = seconds2hours(mytime)
+    days_list: list[int] = seconds2days(mytime)
     print(f'''
     Hours: {time_list[0]}
     Minutes: {time_list[1]}
     Seconds: {time_list[2]}''')
+    print("==========================")
+    print(f'''
+    Days: {days_list[0]}
+    Hours: {days_list[1]}
+    Minutes: {days_list[2]}
+    Seconds: {days_list[3]}
+    ''')
 
 
 if __name__ == "__main__":
